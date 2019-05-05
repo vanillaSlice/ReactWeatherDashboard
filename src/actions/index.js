14dab4c9c7da9b2502623aab350e3f3d5c4f27b3
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2019-05-05 16:39:15 +0100

    Make API keys env vars

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
 
 const ROOT_URL = 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast';
-const APP_ID = 'c846518da20ee016d4886d2a895c8245';
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}?q=${city}&appid=${APP_ID}`;
+  const url = `${ROOT_URL}?q=${city}&appid=${process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY}`;
   const request = axios.get(url);
 
   return {
